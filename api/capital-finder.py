aabd4cb5d48a81af967b4c0b7efe95a1812dfe9f
TracyOakley <tloak12@gmail.com>
2023-01-09 23:58:59 +0000

    Add capital finder part

---
+++ api/capital-finder.py
@@ -0,0 +1,27 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+
+        if "capital" in dic:
+            url = "https://restcountries.com/v3.1/capital/"
+            r = requests.get(url + dic["capital"])
+            data = r.json()
+            message = f"{data.capital} is the capital of {data.name.common}"
+
+        else:
+            message = "Give me a country or capital in the world"
+
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+
+        self.wfile.write(message.encode())
+
+        return
